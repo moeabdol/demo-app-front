6eea91744083474657514b32d862c042473c7ea6
moeabdol <admin.r99@gmail.com>
2017-06-20 21:18:10 +0300

    update and delete posts

---
+++ src/app/post/post-update.component.ts
@@ -0,0 +1,40 @@
+import { Component, OnInit } from "@angular/core";
+import { ActivatedRoute } from "@angular/router";
+
+import { IPost } from "./post";
+import { PostService } from "./post.service";
+
+@Component({
+  moduleId: module.id,
+  templateUrl: "post-update.component.html"
+})
+export class PostUpdateComponent implements OnInit {
+  post: IPost;
+  errorMessage: string;
+
+  constructor(
+    private _postService: PostService,
+    private _route: ActivatedRoute
+  ) { }
+
+  ngOnInit() {
+    const id = +this._route.snapshot.params["id"];
+    this.getPost(id)
+  }
+
+  getPost(id: number) {
+    this._postService.getPost(id)
+      .subscribe(
+        post => this.post = post,
+        error => this.errorMessage = <any>error
+      );
+  }
+
+  onSubmit(title: string, body: string) {
+    this._postService.updatePost(this.post.id, {title: title, body: body})
+      .subscribe(
+        data => console.log(data),
+        error => this.errorMessage = <any>error
+      );
+  }
+}
